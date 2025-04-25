59b5175eaa7f4a1bdc220d37eb4f2ebe7eb53cda
Vadim-Ponochevny <Ponovadim67@mail.ru>
2025-04-25 21:13:02 +0700

    task-1

--- app/src/main/java/com/example/laba_6_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/laba_6_kotlin/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.laba_6_kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.laba_6_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = "NewActivity"
+
+        binding.btnShowPic.setOnClickListener {
+            val intent = Intent(this@MainActivity, PicActivity::class.java)
+            intent.putExtra("picLink", "https://i.pinimg.com/736x/d1/44/4d/d1444d96a0f29193c898515177778e49.jpg")
+            startActivity(intent)
         }
     }
 }
